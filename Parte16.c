1fd7b2811deaf33949f2353043467d407b2c437b
Rafael Lavoura <lavourarafael@gmail.com>
2024-12-20 11:13:55 -0300

    Add files via upload

---
+++ Parte16.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{   
+    int cont;
+
+    cont = 1;
+
+    while (cont <= 5)
+    {
+        printf("%d - Linguagem c \n",cont);
+        cont++;
+    }
+    return 0;
+}
