6c10b77f2b25b82c7981f7b193d036c13a36f7e8
Rafael Lavoura <lavourarafael@gmail.com>
2024-12-20 09:38:20 -0300

    Add files via upload

---
+++ Parte2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{   
+    int numero = 10;
+    float numero2 = 6.5;
+    char letra = 'a';
+
+    printf("numero inteiro %d\n", numero);
+    printf("numero real %f\n", numero2);
+    printf("Letra %c\n", letra);
+
+    return 0;
+}
